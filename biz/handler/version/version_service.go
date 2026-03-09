bbc1f24a1b4f4f66449ce784a2ca6139e50495d7
zhangyi <zy84338719@hotmail.com>
2026-03-09 08:00:32 +0800

    fix: 清理不需要的文件和目录

--- biz/handler/version/version_service.go
+++ biz/handler/version/version_service.go
@@ -7,7 +7,6 @@ import (
 
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/yi-nology/git-manage-service/biz/dal/db"
-	version "github.com/yi-nology/git-manage-service/biz/model/version"
 	"github.com/yi-nology/git-manage-service/biz/service/git"
 	"github.com/yi-nology/git-manage-service/pkg/response"
 )
